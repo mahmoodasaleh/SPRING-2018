b632bc950d0c701f47f7501910eb49f7f4edd4a1
Hedaya <hedaya.rafaat@gmail.com>
2018-03-14 19:49:37 +0200

    Directions functions

--- Omni Wheel Robot Project/Ardunino Codes/Blutooth_control/Blutooth_control.ino
+++ Omni Wheel Robot Project/Ardunino Codes/Blutooth_control/Blutooth_control.ino
@@ -9,10 +9,70 @@ int receive1;
 int MW1 = 9;
 int MW2 = 10;
 int MW3 =3;
+int Dir1 = 8;
+int Dir2 = 11;
+int Dir3 = 2;
+
+void forward(){
+  analogWrite(10, 250);
+  analogWrite(9, 250);
+  analogWrite(3, 0);
+  digitalWrite(11, HIGH);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+
+}
+void back(){
+  analogWrite(10, 250);
+  analogWrite(9, 250);
+  analogWrite(3, 0);
+  digitalWrite(11, LOW);
+  digitalWrite(8, HIGH);
+  digitalWrite(2, LOW);
+
+}
+
+void right(){
+  analogWrite(10, 250);
+  analogWrite(9, 0);
+  analogWrite(3, 0);
+  digitalWrite(11, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+
+}
+
+void left(){
+  analogWrite(10, 0);
+  analogWrite(9, 250);
+  analogWrite(3, 0);
+  digitalWrite(11, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+
+}
+
+void rotate(){
+  analogWrite(10, 250);
+  analogWrite(9, 250);
+  analogWrite(3, 250);
+  digitalWrite(11, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+}
+
+void circle(){
+  analogWrite(10, 250);
+  analogWrite(9, 250);
+  analogWrite(3, 0);
+  digitalWrite(11, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(2, LOW);
+}
 
 
 
-SoftwareSerial OmniBlurtooth(A2, A3);
+SoftwareSerial OmniBlurtooth(A2, A3); //A2 Rx A3 Tx
 
 void setup() {
     Serial.begin(9600);          // initiete the signal for blutooth moule:
@@ -40,21 +100,23 @@ void loop() {
 
   if(receive1=='F')
   {
-
+  forward();
  // Omni.setCarAdvance(100);
  //analogWrite(9, 250);
   Serial.println("Robot Moving Forward");
   }
  else if(receive1=='B')
   {  
-    analogWrite(3, 250);
+    back();
+    //analogWrite(3, 250);
 
     //Omni.setCarLeft(100);
   Serial.println("Robot Moving Backward");
   }
   else if(receive1=='R')
   {
-    analogWrite(10, 250);
+    right();
+    //analogWrite(10, 250);
    // Omni.setCarRight(100);
 
 Serial.println("Robot Turning Right");
@@ -63,7 +125,22 @@ Serial.println("Robot Turning Right");
  
   else if(receive1=='L')
   {
+    left();
     Serial.println("Signal is L");
+
+  
+  }
+
+   else if(receive1=='C')
+  {
+    Serial.println("Signal is Circule");
+
+     circle();
+  }
+   else if(receive1=='T')
+  {
+    rotate();
+    Serial.println("Signal is Rotating");
 
   
   }
